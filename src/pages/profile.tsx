c0afe779b686c107f9da7b0518a3719b5c438fb5
yafitb11 <yafitb11@gmail.com>
2025-05-22 21:28:29 +0300

    profile card

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,4 @@
+import { Button, Card } from "flowbite-react";
 import useAuth from "../hooks/useAuth";
 
 const Profile = () => {
@@ -7,13 +8,18 @@ const Profile = () => {
         <div className="flex min-h-screen flex-col items-center justify-start gap-2">
             <h1 className="text-2xl">Profile Page</h1>
             <p className="text-xl dark:text-white">Welcome {user?.name.first}</p>
-            <p className="text-lg dark:text-white">
-                name: {user?.name.first} {user?.name.middle} {user?.name.last} </p>
-            <p className="text-lg dark:text-white">phone: {user?.phone}</p>
-            <p className="text-lg dark:text-white">{user?.email}</p>
-            <p className="text-lg dark:text-white">
-                address:  {user?.address.state}  {user?.address.country}  {user?.address.city}  {user?.address.street}   {user?.address.houseNumber}  {user?.address.zip}
-            </p>
+            {user && <Card className="h-[400px] w-1/4 mycard" imgSrc={user.image.url}>
+
+                <p>Name: {user.name.first} {user.name.middle} {user.name.last}</p>
+                <p> Phone: {user.phone} </p>
+                <p> Email: {user.email} </p>
+                <p> Password:{user.password}  </p>
+                <p> Adress: {user.address.state} {user.address.country} {user.address.city} {user.address.street} {user.address.houseNumber}</p>
+                <p> isBusiness: {user.isBusiness ? "yes" : "no"}</p>
+
+            </Card>}
+
+            <Button>Edit your details</Button>
         </div>
     );
 };
