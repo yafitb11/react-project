db80c48d3b4ef2c7fb7114d6d1f8aff1b78f3f07
yafitb11 <yafitb11@gmail.com>
2025-05-30 23:04:22 +0300

    pagination

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,12 +8,13 @@ import { Tcard } from "../types/cardType";
 import { FaHeart } from "react-icons/fa";
 import { toast } from "react-toastify";
 import useAuth from "../hooks/useAuth";
-
+import { Pagination } from "flowbite-react";
 
 const Home = () => {
     const [cards, setCards] = useState<Tcard[]>([]);
     const nav = useNavigate();
     const [spiner, setspiner] = useState<boolean>(false);
+    const [curPage, setCurPage] = useState<number>(1);
 
     const search = useSelector((state: TRootState) => state.searchSlice.searchWord);
     const { user, autoLogIn } = useAuth();
@@ -82,15 +83,23 @@ const Home = () => {
         }
     };
 
+    const onPageChange = (page: number) => {
+        setCurPage(page);
+    }
 
+    const filterByPage = () => {
+        const start = (curPage - 1) * 12;
+        const end = start + 12;
+        return filterCards().slice(start, end);
+    }
 
     return (
-        <div className="flex flex-col items-center justify-start gap-2 bg-slate-400">
+        <div className="flex flex-col items-center justify-start gap-2 pt-2 pb-2 bg-slate-400">
             <h1 className="text-2xl">Home Page</h1>
             <p className="text-lg">Welcome Home!</p>
 
             <div className="w-[100%] flex gap-3 flex-wrap p-3 justify-center bg-slate-600">
-                {cards && filterCards()?.map((card) => {
+                {cards && filterByPage()?.map((card) => {
                     const isLiked = card.likes.includes(user?._id + "");
                     return (
                         <Card key={card._id} id={card._id} className="h-[500px] w-1/4 mycard" imgSrc={card.image.url}>
@@ -116,6 +125,10 @@ const Home = () => {
             {spiner && (
                 <Spinner color="purple" aria-label="Purple spinner example" />
             )}
+
+            <div className="flex overflow-x-auto sm:justify-center mb-3">
+                <Pagination currentPage={curPage} totalPages={Math.ceil(cards.length / 12)} onPageChange={onPageChange} />
+            </div>
 
         </div>
     );
