919fd76e3cf7aa26a6b28a6dc0065e939f1853ed
yafitb11 <yafitb11@gmail.com>
2025-06-15 18:10:10 +0300

    css almost done

--- src/pages/cardDetails.tsx
+++ src/pages/cardDetails.tsx
@@ -27,27 +27,29 @@ const CardDetails = () => {
     const address = `${card?.address.state} ${card?.address.country} ${card?.address.city} ${card?.address.street} ${card?.address.houseNumber}`;
 
     return (
-        <div className="w-[100%] flex flex-col gap-3 items-center  bg-slate-600 pb-5">
-            <h1 className="text-2xl mt-2">Card Details:</h1>
-            {card && <div className="w-[75%] p-7 pt-5 flex flex-col bg-slate-400">
-                <h1 className="text-2xl text-center">{card.title}</h1>
-                <img src={card.image.url} alt={card.image.alt} className="w-1/3 max-h-96 mt-3 m-auto"></img>
-                <h3>Title: {card.title}</h3>
-                <h3>Subtitle: {card.subtitle}</h3>
-                <h3>Description: {card.description}</h3>
-                <h3>Phone: {card.phone}</h3>
-                <h3>Email: {card.email}</h3>
-                <a href={card.web} target="_blank" className="cursor-pointer">Website: {card.web}</a>
-                <h3>Adress: {address}</h3>
-                <h3> BizNumber: {card.bizNumber}</h3>
-                <div className="m-auto mt-3">
-                    <h2 className="text-center mb-1">Location</h2>
-                    <iframe className="w-[600px] h-[450px] border-0" allowFullScreen loading="lazy"
-                        src={`https://maps.google.com/maps?q=${address}&output=embed`}
-                    >
-                    </iframe>
-                </div>
-            </div>}
+        <div className="w-[100%] flex flex-col gap-3 pt-2 items-center  bg-blue-300 dark:bg-slate-400">
+            <h1 className="text-3xl m-1">Card Details:</h1>
+            <div className="w-full flex justify-center bg-blue-100 dark:bg-slate-800">
+                {card && <div className="w-[75%] p-5 my-4 rounded-[10px] flex flex-col bg-lightblue dark:bg-slate-500">
+                    <h1 className="text-2xl text-center">{card.title}</h1>
+                    <img src={card.image.url} alt={card.image.alt} className="w-1/3 max-h-96 mt-3 m-auto"></img>
+                    <h3>Title: {card.title}</h3>
+                    <h3>Subtitle: {card.subtitle}</h3>
+                    <h3>Description: {card.description}</h3>
+                    <h3>Phone: {card.phone}</h3>
+                    <h3>Email: {card.email}</h3>
+                    <a href={card.web} target="_blank" className="cursor-pointer">Website: {card.web}</a>
+                    <h3>Adress: {address}</h3>
+                    <h3> BizNumber: {card.bizNumber}</h3>
+                    <div className="m-auto mt-3">
+                        <h2 className="text-center mb-1">Location</h2>
+                        <iframe className="w-[600px] h-[450px] border-0" allowFullScreen loading="lazy"
+                            src={`https://maps.google.com/maps?q=${address}&output=embed`}
+                        >
+                        </iframe>
+                    </div>
+                </div>}
+            </div>
         </div>
     );
 };
